f097ccd8c7349894bf677dd4cc28bebde31b5639
thirupathimudigiri <163870838+thirupathimudigiri@users.noreply.github.com>
2024-08-02 12:12:07 +0530

    Add files via upload

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,17 +1,42 @@
 /* GET 'home' page */
 const homelist = (req, res) => {
-res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', {
+    title: 'Course Management System',
+    pageHeader: {
+    title: 'Course Management System',
+    strapline: 'lets full fill your passion together! ' 
+    },
+    locations: [{
+    name: 'java from Scratch',
+    tutor: 'abdul khaleel',
+    rating: 3,
+    contents: ['java basics', 'java intermediate'],
+    duration: '100m'
+    },{
+    name: 'Machine learning for all',
+    tutor: 'mouli majeed',
+    rating: 5,
+    contents: ['basics', 'intermediate'],
+    duration: '100m'
+    },{
+        name: 'AI ',
+        tutor: 'nani nalli',
+        rating: 4,
+        contents: ['basics', 'intermediate','advanced'],
+        duration: '200m'
+}]
+});
 };
     /* GET 'Location info' page */
     const locationInfo = (req, res) => {
-    res.render('location-info', { title: 'Location info' });
+    res.render('location-info', { title: 'course details' });
     };
     /* GET 'Add review' page */
     const addReview = (req, res) => {
     res.render('location-review-form', { title: 'Add review' });
     };
-module.exports = {
-    homelist,
-    locationInfo,
-    addReview
-};
+    module.exports = {
+        homelist,
+        locationInfo,
+        addReview
+        };
